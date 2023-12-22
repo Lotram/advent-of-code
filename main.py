8e0d3d4d72efab9a7c59e736174e6d50a97a347a
Laurent Tramoy <laurenttramoy@gmail.com>
2023-12-22 19:10:23 +0100

    fix python main

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ app = typer.Typer(no_args_is_help=True)
 
 @app.command()
 def main(day: int, part: int, use_example: bool = False, submit: bool = False):
+    year = tomllib.loads(Path(".cargo/config.toml").read_text())["env"]["AOC_YEAR"]
     if use_example and submit:
         raise ValueError("Cannot use both --use-example and --submit")
     file_name = f"day_{day:02}"
     data_dir = "examples" if use_example else "inputs"
-    data = Path("data") / data_dir / f"{day:02}.txt"
+    data = Path("data") / data_dir / year / f"{day:02}.txt"
     package = importlib.import_module(f"src.bin.{file_name}")
     func = getattr(package, f"part_{part}")
     result = func(data.read_text())
     print(result)
     if submit:
-        year = tomllib.loads(Path(".cargo/config.toml").read_text())["env"]["AOC_YEAR"]
         subprocess.run(f"aoc submit -y {year} -d {day:02} {part} {result}", shell=True)
 
 
