1eca835e43f48c890ff24fe73d7fd786f12273ba
Laurent Tramoy <laurenttramoy@gmail.com>
2024-01-17 18:51:53 +0100

    Add 12 and 13

--- src/bin/day_13.py
+++ src/bin/day_13.py
@@ -1,17 +1,18 @@
 import re
 from collections import defaultdict
 
-from .held_karp import shortest_hamiltionan_path
+from .held_karp import held_karp_bitmask, shortest_hamiltionan_path
 
-pattern = re.compile(r"(?P<start>\w+) to (?P<stop>\w+) = (?P<dist>\d+)")
+pattern = re.compile(
+    r"(?P<start>\w+) would (?P<sign>gain|lose) (?P<value>\d+) happiness units by sitting next to (?P<stop>\w+)."
+)
 
 
 def build_mapping(lines):
     distances = defaultdict(dict)
     for line in lines:
-        start, stop, dist = pattern.match(line).groups()
-        distances[start][stop] = int(dist)
-        distances[stop][start] = int(dist)
+        start, sign, value, stop = pattern.match(line).groups()
+        distances[start][stop] = int(value) * (-1 if sign == "lose" else 1)
     return distances
 
 
@@ -21,11 +22,9 @@ def build_matrix(distances):
     dists = [[0] * N for _ in range(N)]
     for city, _distances in distances.items():
         for other, distance in _distances.items():
-            dists[cities[city]][cities[other]] = distance
+            dists[cities[city]][cities[other]] += distance
+            dists[cities[other]][cities[city]] += distance
 
-    for i in range(N):
-        for j in range(i + 1, N):
-            assert dists[i][j] == dists[j][i] != 0
     return dists
 
 
@@ -33,7 +32,7 @@ def part_1(text):
     lines = text.strip().split("\n")
     distances = build_mapping(lines)
     distance_matrix = build_matrix(distances)
-    result, _ = shortest_hamiltionan_path(distance_matrix)
+    result, _ = held_karp_bitmask(distance_matrix, max_value=True)
     return result
 
 
