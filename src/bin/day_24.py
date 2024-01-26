9c7b154a5e2c69dcfa42dd8ebade91001d3fc502
Laurent Tramoy <laurenttramoy@gmail.com>
2024-01-26 12:24:05 +0100

    update day 24

--- src/bin/day_24.py
+++ src/bin/day_24.py
@@ -2,27 +2,22 @@ import math
 from itertools import combinations
 
 
-def part_1(text):
+def solution(text, group_count):
     weights = list(map(int, text.strip().split("\n")))
-    target = sum(weights) // 3
+    target = sum(weights) // group_count
     result = float("inf")
-    for number in range(1, len(weights) // 3 + 1):
+    for number in range(1, len(weights) // group_count + 1):
         for _weights in combinations(weights, number):
             if sum(_weights) == target:
                 result = min(result, math.prod(_weights))
         if result < float("inf"):
             break
     return result
+
+
+def part_1(text):
+    return solution(text, 3)
 
 
 def part_2(text):
-    weights = list(map(int, text.strip().split("\n")))
-    target = sum(weights) // 4
-    result = float("inf")
-    for number in range(1, len(weights) // 4 + 1):
-        for _weights in combinations(weights, number):
-            if sum(_weights) == target:
-                result = min(result, math.prod(_weights))
-        if result < float("inf"):
-            break
-    return result
+    return solution(text, 4)
