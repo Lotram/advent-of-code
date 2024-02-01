19c00ef4e7107f4b34f5c67eb3d5b83563f2b0d7
Laurent Tramoy <laurenttramoy@gmail.com>
2024-02-01 13:56:28 +0100

    add days 7 and 8

---
+++ src/bin/day_08.py
@@ -0,0 +1,32 @@
+import re
+
+import numpy as np
+
+from .grid import Grid
+
+
+def part_1(text):
+    screen = np.zeros((6, 50), dtype=bool)
+    lines = text.strip().split("\n")
+    pattern = re.compile(r"\d+")
+    for line in lines:
+        x, y = map(int, pattern.findall(line))
+        if line.startswith("rotate row"):
+            screen[x] = np.roll(screen[x], shift=y)
+        elif line.startswith("rotate column"):
+            screen[:, x] = np.roll(screen[:, x], shift=y)
+        elif line.startswith("rect"):
+            screen[:y, :x] = True
+        else:
+            raise ValueError(line)
+        print()
+
+    Grid(arr=screen).print()
+
+    result = screen.sum()
+    return result
+
+
+def part_2(text):
+    result = "EOARGPHYAO"
+    return result
