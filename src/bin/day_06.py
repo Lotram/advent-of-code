a26ff57b5f211864ab0d12ec8adca01af8e0e2fc
Laurent Tramoy <laurenttramoy@gmail.com>
2024-02-19 12:59:49 +0100

    add days 1 through 7

---
+++ src/bin/day_06.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+def part_1(text):
+    lines = text.strip().split("\n")
+    counters = [Counter() for _ in range(len(lines[0]))]
+    for line in lines:
+        for idx, char in enumerate(line):
+            counters[idx][char] += 1
+    result = "".join(counter.most_common(1)[0][0] for counter in counters)
+    return result
+
+
+def part_2(text):
+    lines = text.strip().split("\n")
+    counters = [Counter() for _ in range(len(lines[0]))]
+    for line in lines:
+        for idx, char in enumerate(line):
+            counters[idx][char] += 1
+    result = "".join(counter.most_common(None)[-1][0] for counter in counters)
+    return result
