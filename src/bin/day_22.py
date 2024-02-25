fe0860772afa1927d28cd7daa1858c15ee533f9b
Laurent Tramoy <laurenttramoy@gmail.com>
2024-02-25 18:32:11 +0100

    add 22

---
+++ src/bin/day_22.py
@@ -0,0 +1,81 @@
+import re
+from bisect import bisect
+from operator import attrgetter, neg
+from typing import NamedTuple
+
+import numpy as np
+
+from .grid import Grid, Point
+
+PATTERN = re.compile(r"\d+")
+
+
+class Disk(NamedTuple):
+    used: int
+    available: int
+
+
+class Node(NamedTuple):
+    point: Point
+    disk: Disk
+
+
+def parse(text: str) -> list[Node]:
+    disks = []
+    for line in text.strip().split("\n")[2:]:
+        row, col, total, used, available, used_percent = map(int, PATTERN.findall(line))
+        disks.append(Node(Point(row, col), Disk(used, available)))
+
+    return disks
+
+
+def part_1(text):
+    disks = [node.disk for node in parse(text)]
+    availables = sorted(map(attrgetter("available"), disks), reverse=True)
+    result = 0
+    for disk in disks:
+        if not disk.used:
+            continue
+        result += bisect(availables, -disk.used, key=neg) - (
+            disk.used <= disk.available
+        )
+    return result
+
+
+BIG_DISK_THRESHOLD = 100
+
+
+def get_value(node):
+    if node.disk.used >= BIG_DISK_THRESHOLD:
+        return "#"
+    elif node.disk.used == 0:
+        return "_"
+    else:
+        return "."
+
+
+def part_2(text):
+    """
+    The input as the same shape as the exemple:
+    * 1 empty node
+    * several nodes too big to move
+    * all other nodes can be moved, but can't accept data from two other nodes at the same time
+
+    There is a line of full disks between the empty disk and the target that we need
+    to bypass
+    """
+    nodes = parse(text)
+    row_size = nodes[-1].point.row + 1
+    values = [get_value(node, row_size) for node in nodes]
+    grid = Grid(np.array(np.split(np.array(values), row_size)))
+    empty = grid.find("_")
+    first_full = grid.find("#")
+    assert all(grid[Point(row)] == "#" for row in range(first_full.row, row_size))
+    first_available = Point(first_full.row - 1, first_full.col)
+    result = int(
+        (empty - first_available).norm(p=1)
+        + (first_available - Point(row_size - 2, 0)).norm(p=1)
+        + 5 * (row_size - 2)
+        + 1
+    )
+    return result
