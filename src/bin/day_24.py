da30d5c2540f1e7653f437777d5a1f84ee194bae
Laurent Tramoy <laurenttramoy@gmail.com>
2024-02-26 20:57:00 +0100

    add 24 and 25

---
+++ src/bin/day_24.py
@@ -0,0 +1,86 @@
+from itertools import starmap
+
+import numpy as np
+
+from .dijkstra import dijkstra
+from .grid import Grid, Point
+from .held_karp import held_karp_bitmask
+
+
+def stop_cond_func(nodes):
+    visited = set()
+
+    def stop_condition(current):
+        if current in nodes:
+            visited.add(current)
+            return visited == nodes
+        return False
+
+    return stop_condition
+
+
+def get_neighbour_func(grid: Grid):
+    def get_neighbours(node: Point):
+        for neighbour, value in grid.neighbours(node):
+            if value != "#":
+                yield (neighbour, 1)
+
+    return get_neighbours
+
+
+def part_1(text):
+    grid = Grid(np.array(list(map(list, text.strip().split("\n")))))
+    nodes = sorted(
+        starmap(Point, zip(*np.where(np.vectorize(lambda x: x.isdigit())(grid.arr)))),
+        key=lambda node: int(grid[node]),
+    )
+    get_neighbours = get_neighbour_func(grid)
+
+    distances = [[0] * len(nodes) for _ in range(len(nodes))]
+
+    for idx, node in enumerate(nodes[:-1]):
+        stop_cond = stop_cond_func(set(nodes[idx + 1 :]))
+        dists_from_node = dijkstra(node, stop_cond, get_neighbours, return_all=True)
+        for other_idx, other_node in enumerate(nodes[idx + 1 :], idx + 1):
+            distances[idx][other_idx] = dists_from_node[other_node]
+            distances[other_idx][idx] = dists_from_node[other_node]
+
+    max_value = len(nodes) * max(distance for dists in distances for distance in dists)
+
+    # add a node with a distance of 0 to the start, and max_value to_all other nodes
+    distances_with_dummy = [
+        [0, *[max_value] * (len(distances))],
+        [0, *distances[0]],
+        *[[max_value, *row] for row in distances[1:]],
+    ]
+
+    cost, path = held_karp_bitmask(distances_with_dummy)
+    cost -= max_value
+
+    # path = [idx - 1 for idx in path[1:]]
+    # if path[0] != 0:
+    #     path = path[::-1]
+
+    return cost
+
+
+def part_2(text):
+    grid = Grid(np.array(list(map(list, text.strip().split("\n")))))
+    nodes = sorted(
+        starmap(Point, zip(*np.where(np.vectorize(lambda x: x.isdigit())(grid.arr)))),
+        key=lambda node: int(grid[node]),
+    )
+    get_neighbours = get_neighbour_func(grid)
+
+    distances = [[0] * len(nodes) for _ in range(len(nodes))]
+
+    for idx, node in enumerate(nodes[:-1]):
+        stop_cond = stop_cond_func(set(nodes[idx + 1 :]))
+        dists_from_node = dijkstra(node, stop_cond, get_neighbours, return_all=True)
+        for other_idx, other_node in enumerate(nodes[idx + 1 :], idx + 1):
+            distances[idx][other_idx] = dists_from_node[other_node]
+            distances[other_idx][idx] = dists_from_node[other_node]
+
+    cost, path = held_karp_bitmask(distances)
+
+    return cost
---
+++ src/bin/day_25.py
@@ -0,0 +1,80 @@
+def _solution(lines, registers):
+    cursor = 0
+    loops = 0
+
+    def get_value(x):
+        return int(x) if x.isdigit() else registers[x]
+
+    while cursor < len(lines):
+        line = lines[cursor]
+        match line:
+            case ["cpy", x, y]:
+                registers[y] = get_value(x)
+
+            case ["inc", x]:
+                registers[x] += 1
+
+            case ["dec", x]:
+                registers[x] -= 1
+
+            case ["jnz", x, y]:
+                if get_value(x) != 0:
+                    cursor += int(y)
+                    continue
+            case ["out", x]:
+                print(registers[x], end="")
+                loops += 1
+                if loops >= 20:
+                    return ""
+
+        cursor += 1
+
+    result = registers
+    return result
+
+
+def solution(x):
+    """
+    get the first int greater than x and written as "101010...10", and returns its
+    substraction by x
+    """
+    result = 2
+    while result < x:
+        result = (result << 2) + 2
+    return result - x
+
+
+def part_1(text):
+    """
+    The set of instructions is equivalent to:
+
+    while True:
+        a = value + seed
+        while a:
+            a, b = a // 2, a % 2
+            print(b, end="")
+
+
+    where seed depends on the input and value is the initial value of a
+    """
+    lines = [line.split() for line in text.strip().split("\n")[:3]]
+    seed = int(lines[1][1]) * int(lines[2][1])
+    return solution(seed)
+
+
+def f(x):
+    loops = 0
+    while True:
+        a = x
+        while a:
+            a, b = a // 2, a % 2
+            print(b, end="")
+            loops += 1
+            if loops >= 20:
+                return
+
+
+def part_2(text):
+    registers = dict.fromkeys("abcd", 0)
+    registers["c"] = 1
+    return solution(text, registers)
