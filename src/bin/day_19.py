78d0390df83c2758e3f82a4933883b9b88a3e895
Laurent Tramoy <laurenttramoy@gmail.com>
2024-03-26 13:54:09 +0100

    add 19

---
+++ src/bin/day_19.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from .grid import DIRECTIONS, SOUTH, Grid
+
+
+def solve(text):
+    arr = np.array([list(line) for line in text.split("\n") if line])
+    grid = Grid(arr)
+    position = grid.find("|")
+    direction = SOUTH
+    letters = []
+    steps = 0
+    while (char := grid[position]) != " ":
+        steps += 1
+        if char.isalpha():
+            letters.append(char)
+
+        if char == "+":
+            direction = next(
+                dir_
+                for dir_ in DIRECTIONS
+                if dir_ != -direction and grid[position + dir_] != " "
+            )
+
+        position += direction
+    return "".join(letters), steps
+
+
+def part_1(text):
+    result, _ = solve(text)
+    return result
+
+
+def part_2(text):
+    _, result = solve(text)
+    return result
