0044fd1e94ca5d0c3cde21340873e30d34008e07
Laurent Tramoy <laurenttramoy@gmail.com>
2024-03-26 22:27:57 +0100

    add 15 to 18

---
+++ src/bin/day_17.py
@@ -0,0 +1,19 @@
+def part_1(text):
+    steps = int(text.strip())
+    buffer = [0]
+    current_position = 0
+    for idx in range(1, 2018):
+        current_position = (current_position + steps) % idx + 1
+        buffer.insert(current_position, idx)
+    result = buffer[(buffer.index(2017) + 1) % len(buffer)]
+    return result
+
+
+def part_2(text):
+    steps = int(text.strip())
+    current_position = 0
+    for idx in range(1, 5_000_001):
+        current_position = (current_position + steps) % idx + 1
+        if current_position == 1:
+            result = idx
+    return result
