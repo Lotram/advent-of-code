9bc0cb4851f4eb1055ce8822824d213790318d75
Laurent Tramoy <laurenttramoy@gmail.com>
2024-12-16 19:50:44 +0100

    add 2019 - 14 part 2

--- pycli/src/year_2019/day_14.py
+++ pycli/src/year_2019/day_14.py
@@ -1,6 +1,7 @@
 import math
 import re
 from collections import defaultdict
+from itertools import count
 
 import networkx as nx
 
@@ -17,35 +18,55 @@ def parse(text):
     reactions = {}
     graph = nx.DiGraph()
     for line in text.strip().splitlines():
-        *inputs, (result_count, result) = map(parse_chemical, pattern.findall(line))
-        reactions[result] = (inputs, result_count)
+        *inputs, (produced, result) = map(parse_chemical, pattern.findall(line))
+        reactions[result] = (inputs, produced)
         for _, _input in inputs:
             graph.add_edge(_input, result)
 
     return reactions, graph
 
 
-def part_1(text, example: bool = False):
-    reactions, graph = parse(text)
+def solve(nodes, reactions, fuel_needed):
     result = 0
     needs = defaultdict(int)
-    needs["FUEL"] = 1
-    nodes = nx.dfs_postorder_nodes(graph, "ORE")
+    needs["FUEL"] = fuel_needed
     for node in nodes:
         needed = needs[node]
         if node == "ORE":
             result += needed
             continue
 
-        inputs, result_count = reactions[node]
-        reaction_count = math.ceil(needed / result_count)
+        inputs, produced = reactions[node]
+        reaction_count = math.ceil(needed / produced)
 
-        for count, _input in inputs:
-            needs[_input] += count * reaction_count
+        for _count, _input in inputs:
+            needs[_input] += _count * reaction_count
 
+    return result
+
+
+def part_1(text, example: bool = False):
+    reactions, graph = parse(text)
+    nodes = nx.dfs_postorder_nodes(graph, "ORE")
+    result = solve(nodes, reactions, fuel_needed=1)
     return result
 
 
 def part_2(text, example: bool = False):
-    result = None
-    return result
+    reactions, graph = parse(text)
+    nodes = list(nx.dfs_postorder_nodes(graph, "ORE"))
+    trillion = 1000000000000
+    value = solve(nodes, reactions, 1)
+    value = trillion // value
+    iterations = 0
+    while True:
+        ore_needed = solve(nodes, reactions, value)
+        iterations += 1
+        if ore_needed > trillion:
+            print(iterations)
+            return value - 1
+        if (trillion / ore_needed) > 1.1:
+            print(iterations, value, trillion / ore_needed)
+            value = int(value * trillion / ore_needed)
+        else:
+            value += 1
