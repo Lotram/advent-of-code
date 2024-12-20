157cf34a164107e6ae3a4d347712c5570d8859f1
Laurent Tramoy <laurenttramoy@gmail.com>
2024-12-20 22:27:13 +0100

    add 2024 - 20

---
+++ pycli/src/year_2024/day_20.py
@@ -0,0 +1,68 @@
+from collections import deque
+
+from pycli.src.grid import Grid, Vector
+
+
+def build_path(grid):
+    start = grid.find("S")
+    end = grid.find("E")
+    grid.replace("E", ".")
+    grid.replace("S", ".")
+    path = deque([start, start])
+    while path[-1] != end:
+        path.append(
+            next(
+                _position
+                for _position, value in grid.neighbours(path[-1])
+                if value == "." and _position != path[-2]
+            )
+        )
+
+    path.popleft()
+    return deque([complex(*position) for position in path])
+
+
+def solve(path, min_goal, max_distance):
+    all_directions = {
+        idx: [
+            complex(x, y)
+            for x in range(-max_distance, max_distance + 1)
+            for y in range(-max_distance, max_distance + 1)
+            if abs(x) + abs(y) == idx
+        ]
+        for idx in range(1, max_distance + 1)
+    }
+    too_close = deque([])
+    for _ in range(min_goal):
+        too_close.append(path.popleft())
+
+    idx = 0
+    result = 0
+    path_dict = {position: idx for idx, position in enumerate(path)}
+    while path:
+        position = too_close.popleft()
+        for norm, _all_directions in all_directions.items():
+            for direction in _all_directions:
+                if path_dict.get(position + direction, -1) >= idx + norm:
+                    result += 1
+
+        item = path.popleft()
+        too_close.append(item)
+        idx += 1
+    return result
+
+
+def part_1(text, example: bool = False):
+    grid = Grid.from_text(text)
+    min_goal = 4 if example else 100
+    path = build_path(grid)
+    result = solve(path, min_goal, 2)
+    return result
+
+
+def part_2(text, example: bool = False):
+    grid = Grid.from_text(text)
+    min_goal = 50 if example else 100
+    path = build_path(grid)
+    result = solve(path.copy(), min_goal, 20)
+    return result
